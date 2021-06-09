5d3f6a8cc039e2a266b0a8346a73f5fcc6edc291
Lars Kahlert <lars.kahlert@codecentric.de>
2021-06-09 07:41:00 +0200

    Init commit of the main project structure

---
+++ src/SchlauHaus.WebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace SchlauHaus.WebApi.Extensions
+{
+    internal static class ServiceExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddAndConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options
+                    .AddPolicy("CorsPolicy",
+                        builder => builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+            });
+
+            return services;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddAndConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "SchlauHaus.WebApi", Version = "v1"});
+            });
+
+            return services;
+        }   
+    }
+}
